ecadc105c6e30eb3364f46ac87c99423c80cd2f0
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-10-22 16:32:04 -0700

    redness

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Bitmap/Bitmap.h"
+#include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
 
@@ -8,6 +8,41 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
+
+if(argc!=2)
+{
+cout<<"Please specify one image file!\n";
+}
+
+string filename = argv[1];
+Bitmap image;
+image.open(filename);
+
+
+if(image.isImage())
+{
+  vector < vector <Pixel> > imagePixels = image.toPixelMatrix();  
+  cout<<argv[1]<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" pixels wide\n";
   
+  for(int row=0; row<imagePixels.size(); row++)
+  {
+    for(int col=0; col<imagePixels[row].size(); col++)   
+    {
+      Pixel color;
+      color = imagePixels[row][col];
+      color.green = 0;
+      color.blue = 0;
+      imagePixels[row][col] = color;
+    }
+  }
+  image.fromPixelMatrix(imagePixels);
+  image.save("redness.bmp");
+}
+else
+{
+  cout<<"Image file must be a bitmap with a 24-bit color depth.\n";
+}
+
   return 0;
 }
+
